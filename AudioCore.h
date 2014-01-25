d5a40cc5aed6a3411d78322a9acbb22d343b5b5b
COUNTERKILL <COUNTERKILL@inbox.ru>
2014-01-25 10:37:30 +0600

    Аудио ядро

---
+++ AudioCore.h
@@ -0,0 +1,16 @@
+//AudioCore.h
+#ifndef AudioCore_h
+#define AudioCore_h
+#include "bass.h"// есть под linux
+
+bool InitAudio(); // инициализация аудио
+class Audio{
+public:
+	Audio(char* path); // путь к файлу. В винде с двойными слэшами
+	void Play(); // проиграть
+	~Audio();
+	char* path;
+private:
+	HSAMPLE sample;
+};
+#endif
