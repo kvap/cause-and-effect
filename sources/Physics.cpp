6a94bd33d4a7c5758870937cbb29b3f32029f5a4
Constantin S. Pan <kvapen@gmail.com>
2014-01-28 06:15:18 +0600

    Fixed the "sliding and sticking" bug (existing contacts did not get the updated friction).

--- sources/Physics.cpp
+++ sources/Physics.cpp
@@ -3,51 +3,53 @@
 Physics::Physics(GameObject* gameObject, Physics::Type type)
     : type(type)
 {
-    this->objectBodyDef = new b2BodyDef();
-    this->objectBodyDef->position.Set(gameObject->getPosition().x, gameObject->getPosition().y);
+	this->objectBodyDef = new b2BodyDef();
+	this->objectBodyDef->position.Set(gameObject->getPosition().x, gameObject->getPosition().y);
 
-    this->objectPolygonShape = new b2PolygonShape();
-    this->objectPolygonShape->SetAsBox(gameObject->getSize().x, gameObject->getSize().y);
+	this->objectPolygonShape = new b2PolygonShape();
+	this->objectPolygonShape->SetAsBox(gameObject->getSize().x, gameObject->getSize().y);
 
-    if (type == Physics::DYNAMIC)
-    {
-        this->objectBodyDef->type = b2_dynamicBody;
+	if (type == Physics::DYNAMIC)
+	{
+		this->objectBodyDef->type = b2_dynamicBody;
 
-        this->objectFixtureDef = new b2FixtureDef();
-        this->objectFixtureDef->shape = this->objectPolygonShape;
-        this->objectFixtureDef->density = 10.5f;
-        this->objectFixtureDef->restitution = 0.0f;
-        this->objectFixtureDef->friction = 15.0f;
-    }
+		this->objectFixtureDef = new b2FixtureDef();
+		this->objectFixtureDef->shape = this->objectPolygonShape;
+		this->objectFixtureDef->density = 10.5f;
+		this->objectFixtureDef->restitution = 0.0f;
+		this->objectFixtureDef->friction = 15.0f;
+	}
 }
 
 Physics::~Physics()
 {
-    delete this->objectBodyDef;
-    delete this->objectPolygonShape;
-    if (this->type == Physics::DYNAMIC)
-        delete this->objectFixtureDef;
+	delete this->objectBodyDef;
+	delete this->objectPolygonShape;
+	if (this->type == Physics::DYNAMIC)
+		delete this->objectFixtureDef;
 }
 
 void Physics::attach(b2World* physicsWorld)
 {
-    this->objectBody = physicsWorld->CreateBody(this->objectBodyDef);
-    if (this->type == Physics::STATIC)
-    {
-        this->objectBody->CreateFixture(this->objectPolygonShape, 10.0f);
-    }
-    else // dynamic
-    {
-        this->objectBody->CreateFixture(this->objectFixtureDef);
-    }
+	this->objectBody = physicsWorld->CreateBody(this->objectBodyDef);
+	if (this->type == Physics::STATIC)
+	{
+		this->objectBody->CreateFixture(this->objectPolygonShape, 10.0f);
+	}
+	else // dynamic
+	{
+		this->objectBody->CreateFixture(this->objectFixtureDef);
+	}
 }
 
-void Physics::setFriction(double friction)
-{
-	if (type == Physics::DYNAMIC)
-	{
-		this->objectFixtureDef->friction = friction;
-		this->objectBody->CreateFixture(this->objectFixtureDef);
+void Physics::setFriction(double friction) {
+	for (b2Fixture* f = objectBody->GetFixtureList(); f; f = f->GetNext()) {
+		// this will not change the friction of existing contacts
+		f->SetFriction(friction);
+	}
+	for (b2ContactEdge* ce = objectBody->GetContactList(); ce; ce = ce->next) {
+		// this is to update the existing contacts
+		ce->contact->ResetFriction();
 	}
 }
 
@@ -60,14 +62,14 @@ double Physics::getFriction()
 
 Point Physics::getPosition()
 {
-    b2Vec2 bodyPosition = this->objectBody->GetPosition();
-    Point position(bodyPosition.x, bodyPosition.y);
-    return position;
+	b2Vec2 bodyPosition = this->objectBody->GetPosition();
+	Point position(bodyPosition.x, bodyPosition.y);
+	return position;
 }
 
 float32 Physics::getAngle()
 {
-    return this->objectBody->GetAngle();
+	return this->objectBody->GetAngle();
 }
 
 b2Body* Physics::getBody()
